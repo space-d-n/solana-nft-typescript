41b80660ac64a1b80308307acab2e6b5f0b0a2b5
space-d-n <39944650+space-d-n@users.noreply.github.com>
2022-06-06 14:53:01 +0400

    contract to transfer token (v2)

--- src/token-transfer.ts
+++ src/token-transfer.ts
@@ -1,106 +1,78 @@
-import {clusterApiUrl, Connection, Keypair, PublicKey, sendAndConfirmTransaction, Transaction,} from "@solana/web3.js";
-import {
-  createAssociatedTokenAccount,
-  createAssociatedTokenAccountInstruction, createTransferCheckedInstruction,
-  getAssociatedTokenAddress,
-} from "@solana/spl-token";
+import {clusterApiUrl, Connection, Keypair, PublicKey} from "@solana/web3.js";
+import {getOrCreateAssociatedTokenAccount, transferChecked} from "@solana/spl-token";
 import * as bs58 from "bs58";
 
 (async () => {
   // connection
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-  // wallet2 - keygen
-  const fromWallet = Keypair.fromSecretKey(
-    bs58.decode(
-      "fBSh6QDa3uzDFEDr4bf1wir7cQYKn6PgLEmLSihKAPxFyoXVUDxLC4qzndBTtoMZcu67waF4gJyZcGyP5Am845B"
-    )
-  );
-
   // wallet3
-  const toWallet = Keypair.fromSecretKey(
+  const owner = Keypair.fromSecretKey(
     bs58.decode(
       "fQ7PnQj1qYtztdYSUe9zHdJm24oVXDcCZqEzMrfhj6sWJGKCXDuaf3mFLLyTRRt81zEs1FL9pcYL9kjp4C4kp7P"
     )
   );
 
-  const mintPubkey = new PublicKey(
-    "96mGeFMgdrZwEVPLKhpM5zTKmbR9odyhpEUbpW9pTckx"
+  const ownerBalance = await connection.getBalance(owner.publicKey)
+  console.log(`owner balance ${ownerBalance}`)
+
+  // wallet1
+  const receiver = Keypair.fromSecretKey(
+    bs58.decode(
+      "5eBU3GmWaNEsmMpywu8i2df8KLLBYS8kgTXSp8acwuTz2nV1BkUUQuHAXTHcc5xosGozmevXBg8PBZUJkbhkEYLz"
+    )
   );
 
-  // 1) use build-in function
-  // {
-  //   let ata = await createAssociatedTokenAccount(
-  //     connection, // connection
-  //     feePayer, // fee payer
-  //     mintPubkey, // mint
-  //     toWallet.publicKey // owner,
-  //   );
-  //   console.log(`ATA: ${ata.toBase58()}`);
-  // }
+  const receiverBalance = await connection.getBalance(receiver.publicKey)
+  console.log(`receiver balance ${receiverBalance}`)
 
-  // or
+  const mintPubkey = new PublicKey(
+    "6Qxc5ERNnGMTWU5eBbqcf8KE3XAHo5ZoW667Q92UEa5z"
+  );
 
-  // 2) composed by yourself
-  {
-    // calculate ATA
-    let ata = await getAssociatedTokenAddress(
-      mintPubkey, // mint
-      toWallet.publicKey // owner
-    );
-    console.log(`ATA: ${ata.toBase58()}`);
+  const ownerTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    owner,
+    mintPubkey,
+    owner.publicKey
+  )
+  console.log(`owner ATA: ${ownerTokenAccount.address.toBase58()}`);
+  console.log(`owner ATA: ${ownerTokenAccount.owner.toBase58()}`);
+  console.log(`owner ATA: ${ownerTokenAccount.amount}`);
+  console.log(`owner ATA: ${ownerTokenAccount.mint.toBase58()}`);
 
-    // if your wallet is off-curve, you should use
-    // let ata = await getAssociatedTokenAddress(
-    //   mintPubkey, // mint
-    //   alice.publicKey // owner
-    //   true, // allowOwnerOffCurve
-    // );
+  const ownerTokenAccountBalance = await connection.getBalance(ownerTokenAccount.address)
+  console.log(`ownerTokenAccount balance ${ownerTokenAccountBalance}`)
 
-    let tx = new Transaction()
 
-    tx.add(
-      createAssociatedTokenAccountInstruction(
-        fromWallet.publicKey, // payer
-        ata, // ata
-        toWallet.publicKey, // owner
-        mintPubkey // mint
-      )
-    );
-    console.log(`txhash: ${await connection.sendTransaction(tx, [fromWallet])}`);
+  // calculate ATA
+  let receiverTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    owner,
+    mintPubkey, // mint
+    receiver.publicKey // owner
+  );
+  console.log(`receiver ATA: ${receiverTokenAccount.address.toBase58()}`);
 
-    tx.add(
-      createTransferCheckedInstruction(
-        mintPubkey, // from (should be a token account)
-        mintPubkey, // mint
-        ata, // to (should be a token account)
-        fromWallet.publicKey, // from's owner
-        1e8, // amount, if your deciamls is 8, send 10^8 for 1 token
-        8 // decimals
-      )
-    );
-    console.log(
-      `txhash: ${await connection.sendTransaction(tx, [
-        fromWallet,
-        toWallet /* fee payer + owner */,
-      ])}`
-    );
+  const receiverTokenAccountBalance = await connection.getBalance(receiverTokenAccount.address)
+  console.log(`receiverTokenAccount balance ${receiverTokenAccountBalance}`)
 
-    await sendAndConfirmTransaction(connection, tx, [fromWallet])
-  }
+  // if your wallet is off-curve, you should use
+  // let ata = await getAssociatedTokenAddress(
+  //   mintPubkey, // mint
+  //   alice.publicKey // owner
+  //   true, // allowOwnerOffCurve
+  // );
 
-  // 1) use build-in function
-  // {
-  //   let txhash = await transferChecked(
-  //     connection, // connection
-  //     feePayer, // payer
-  //     tokenAccountXPubkey, // from (should be a token account)
-  //     mintPubkey, // mint
-  //     tokenAccountYPubkey, // to (should be a token account)
-  //     toWallet, // from's owner
-  //     1e8, // amount, if your deciamls is 8, send 10^8 for 1 token
-  //     8 // decimals
-  //   );
-  //   console.log(`txhash: ${txhash}`);
-  // }
+  let txhash = await transferChecked(
+    connection, // connection
+    owner, // payer
+    ownerTokenAccount.address, // from (should be a token account)
+    mintPubkey, // mint
+    receiverTokenAccount.address, // to (should be a token account)
+    owner.publicKey, // from's owner
+    1e8, // amount, if your deciamls is 8, send 10^8 for 1 token
+    8 // decimals
+  );
+  console.log(`txhash: ${txhash}`);
 })();
